af314036adf4d876c7614c582c5eafecfd1f6c18
LlyanaP <pollyanacostasousa@gmail.com>
2024-10-16 18:09:28 -0300

    Puxando do banco para o select

--- Princess/src/components/forms/Select.jsx
+++ Princess/src/components/forms/Select.jsx
@@ -1,33 +1,48 @@
 import style from './Select.module.css'
 
-const Select = ({type, text, name, disabled  }) => {
+const Select = ({type, text, name, disabled, options  }) => {
     return(
         <div className={style.select}>
             <label htmlFor={name}>{text}</label>
 
-            <select name={name} id={name} disabled={disabled }>
+            <select name={name} id={name} disabled={disabled}>
                 <option>Selecione o nicho</option>
-                <option>Cabelo</option>
+
+                {
+                    options.map((option)=>{
+                        return<option>{option.nome_categoria_nicho}</option>
+                    })
+                }
+
+                {/* <option>Cabelo</option>
                 <option>SkinCare</option>
                 <option>BodyCare</option>
-                <option>Maquiagem</option>
+                <option>Maquiagem</option> */}
             </select>
   
         </div>
     )
 }
 
-const SelectAvaliar = ({type, text,name}) => {
+const SelectAvaliar = ({type, text,name, options}) => {
     return(
         <div className={style.select}>
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
                 <option>Avalie o produto</option>
-                <option>💖💖💖💖💖</option>
+
+                {
+                    options.map((option)=>{
+                        return<option>{option.nome_avalie}</option>
+                    })
+                }
+
+
+                {/* <option>💖💖💖💖💖</option>
                 <option>💖💖💖💖</option> 
                 <option>💖💖💖</option> 
                 <option>💖💖</option> 
-                <option>💖</option>     
+                <option>💖</option>      */}
             </select>  
         </div>
     )
--- Princess/src/components/pages/RegisterProduct.jsx
+++ Princess/src/components/pages/RegisterProduct.jsx
@@ -6,29 +6,78 @@ import Button from '../forms/Button'
 
 import Input from '../forms/Input'
 
+import { useState, useEffect } from "react";
+
 import {Select, SelectAvaliar} from '../forms/Select'
 
 const RegisterProduct = () => {
+
+const[nicho, setNicho] = useState([])
+
+const[avalie, setAvalie] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/listagemDeNicho',{
+            method: 'GET',
+            headers:{
+               'Content-Type':'application/jason',
+                'Acess-Control-Allow-Origin':'*',
+                "Acess-Control-Allow-Headers":'*', 
+            }
+        }).then(
+            (resp) => resp.json()
+        ).then(
+            (data) => {
+                console.log('DATA' + data.data.nome_categoria_nicho)
+                setNicho(data.data)
+            }
+        ).catch(
+            (error) =>{console.log(error)}
+        )
+    },[]
+    );
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/Avalie',{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/jason',
+                'Acess-Control-Allow-Origin':'*',
+                "Acess-Control-Allow-Headers":'*', 
+            }
+        }).then(
+            (resp) => resp.json()
+        ).then(
+            (data) => {
+                console.log('DATA' + data.data.nome_avalie)
+                setAvalie(data.data)
+            }
+        ).catch(
+            (error)=>{console.log(error)}
+        )
+    },[]);
+
+
     return(
         <section className={style.register}>
             <h1>Registre seu produto</h1>
 
                 <Input
                     type="text"
-                    name="txt_produto"
+                    name="produto"
                     placeHolder= "Digite o nome do seu produto"
                     text = "Nome do Produto"
                 />
                 <Input
                     type="text"
-                    name="txt_marca"
+                    name="marca"
                     placeHolder= "Digite a marca do produto"
                     text = "Marca"
                 />
 
                 <Input
                     type="text"
-                    name="txt_ean"
+                    name="ean"
                     placeHolder="EAN é um código de barras composto por 13 dígitos para identificar individualmente um objeto ou produto."
                     text="Digite o EAN do produto"
                 />
@@ -36,29 +85,31 @@ const RegisterProduct = () => {
                 <Select
                     name='Nicho'
                     text='Escolha o nicho do produto'
+                    options={nicho}
                 />
                 <Input
                     type="text"
-                    name="txt_utilicorreto"
+                    name="utilizacaoCorreta"
                     placeHolder="Como a industria indica utilizar este produto"
                     text="Utilização da industria"
                 />
                 <Input
                     type='text'
-                    name='text_uso'
+                    name='usoPessoal'
                     placeHolder='Maneira que foi utilizado por sua pessoa'
                     text='Utilização pessoal'
                 />
 
                 <Input
                     type="text"
-                    name="txt_dica"
+                    name="dica"
                     placeHolder="Diferencial que você considera que melhorou sua experiencia"
                     text="Dica"
                 />
                 <SelectAvaliar
                     name="Avaliação"
                     text="Avalie o produto"
+                    options={avalie}
                 />
                 <h2>Contribua adicionando um novo produto e o avaliando </h2>
                 <Button
